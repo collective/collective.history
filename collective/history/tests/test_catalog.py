1e968f8b29ffb4a136b3bc508284c300eb1e3d62
Yann Fouillat <yann.fouillat@gmail.com>
2013-06-12 16:42:11 +0200

    test: add more testing

--- collective/history/tests/test_catalog.py
+++ collective/history/tests/test_catalog.py
@@ -19,6 +19,7 @@ class TestCatalog(base.IntegrationTestCase):
         base.IntegrationTestCase.setUp(self)
         self.portal = self.layer['portal']
         self.request = self.layer['request']
+
         testing.login(self.portal, testing.TEST_USER_NAME)
         testing.setRoles(self.portal, testing.TEST_USER_ID, ['Manager'])
         self.portal.invokeFactory('Document',
@@ -27,17 +28,39 @@ class TestCatalog(base.IntegrationTestCase):
         d1 = self.portal["test-document"]
         d1.setCreationDate(DateTime.DateTime('12.25.2012'))
         d1.processForm()
+        testing.logout()
+
+        acl_users = getToolByName(self.portal, 'acl_users')
+        acl_users.userFolderAddUser('toto', 'password',
+                                    ['Member', 'Manager'], [])
+        testing.login(self.portal, 'toto')
+        self.portal.invokeFactory('Document',
+                                  'test-document-2',
+                                  title="D2")
+        d2 = self.portal["test-document-2"]
+        d2.setCreationDate(DateTime.DateTime('1.25.2013'))
+        d2.processForm()
+
         self.catalog = self.layer['portal'].portal_history_catalog
 
     def test_what_index(self):
         result = self.catalog.searchResults({'what': 'created'})
-        self.assertEqual(len(result), 1)
+        self.assertEqual(len(result), 2)
         brain = result[0]
         try:
             brain.getObject()
         except:
             self.assertTrue(False, "getObject should work")
 
+    def test_who_index(self):
+        result = self.catalog.searchResults({'who': 'toto'})
+
+        self.assertEqual(len(result), 1)
+        try:
+            useraction = result[0].getObject()
+        except:
+            self.assertTrue(False, "getObject should work")
+        self.assertEqual(useraction.who, 'toto')
 
 def test_suite():
     return unittest.defaultTestLoader.loadTestsFromName(__name__)
