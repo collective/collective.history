e20de565a7ee8a622de19a183902ad4b09155126
Yann Fouillat <yann.fouillat@gmail.com>
2013-06-21 14:16:30 +0200

    feat: add i18n

---
+++ collective/history/i18n.py
@@ -0,0 +1,10 @@
+from zope.i18nmessageid import MessageFactory
+
+_ = MessageFactory("collective.history")
+
+_(u"created")
+_(u"modified")
+_(u"removed")
+_(u"copied")
+_(u"statechanged")
+_(u"moved")
